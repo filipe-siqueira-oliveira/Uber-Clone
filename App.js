95d2609a3b88707c5d81d0d8882d4946f1f4a9c6
Filipe Siqueira <fcsiqueira1409@gmail.com>
2023-07-04 16:22:47 -0300

    feat: implement stack navigator

--- App.js
+++ App.js
@@ -1,16 +1,38 @@
+import 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { Provider } from 'react-redux'
-;
+import { Provider } from 'react-redux';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import store from "./store"
 import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <HomeScreen />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen 
+              name='Home' 
+              component={HomeScreen} 
+              options={{
+                headerShown: false
+              }}/>
+            <Stack.Screen 
+              name='Map' 
+              component={MapScreen} 
+              options={{
+                headerShown: false
+              }}
+            />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
